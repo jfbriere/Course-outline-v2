af7034a7ae400cff6cd3fe67970fa5cebec0e5a6
jfbriere <jfbriere@dawsoncollege.qc.ca>
2019-09-27 07:54:31 -0400

    last batch of changes F19

--- batch.sh
+++ batch.sh
@@ -9,9 +9,10 @@
 
 session="_Fall2019"
 
-for dir in *923*; do
+#for dir in *Holly*; do
+
 ## FALL COURSE LIST ##
-#for dir in *BWT* *BXB* *BZE* *BZT* *NYA* *NYB* *NYC* *001* *923* *925* *946*; do
+for dir in *BWT* *BXB* *BZE* *BZT* *NYA* *NYB* *NYC* *001* *923* *925* *946*; do
 
 ## WINTER COURSE LIST ##
 #for dir in *BZA* *BZE* *NYA* *NYB* *NYC* *001* *912* *924* *943* *944* *Astro* ; do
