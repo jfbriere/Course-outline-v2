c8d9cb4d1994720074e9951aa3408c96895069a2
jfbriere <jfbriere@dawsoncollege.qc.ca>
2020-01-08 13:20:32 -0500

    Winter 2020 first set of changes

--- batch.sh
+++ batch.sh
@@ -7,12 +7,12 @@
 # or try
 # dos2unix batch.sh
 
-session="_Fall2019"
+session="_Winter2020"
 
-#for dir in *Holly*; do
+for dir in *Astro*; do
 
 ## FALL COURSE LIST ##
-for dir in *BWT* *BXB* *BZE* *BZT* *NYA* *NYB* *NYC* *001* *923* *925* *946*; do
+#for dir in *BWT* *BXB* *BZE* *BZT* *NYA* *NYB* *NYC* *001* *923* *925* *946*; do
 
 ## WINTER COURSE LIST ##
 #for dir in *BZA* *BZE* *NYA* *NYB* *NYC* *001* *912* *924* *943* *944* *Astro* ; do
